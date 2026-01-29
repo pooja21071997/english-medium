5667614f5885fc37317043032fe39cd2ce0d9966
poojaPersonal <100690066+pooja21071997@users.noreply.github.com>
2026-01-29 14:31:46 +0530

    go to top button

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Footer } from "./components/footer/footer";
 import { Navbar } from "./components/navbar/navbar";
+import { GoToTopComponent } from "./shared/go-to-top/go-to-top";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Footer, Navbar],
+  imports: [RouterOutlet, Footer, Navbar, GoToTopComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
