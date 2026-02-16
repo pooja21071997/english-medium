073fa9b9ea794f14200e45efccfb529fb3489d08
poojaPersonal <100690066+pooja21071997@users.noreply.github.com>
2026-02-16 17:58:38 +0530

    enhancement changes pushed

--- src/app/pages/about/about.ts
+++ src/app/pages/about/about.ts
@@ -7,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrl: './about.css',
 })
 export class About {
+ngAfterViewInit(): void {
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('show');
+        observer.unobserve(entry.target); // optional (prevents re-trigger)
+      }
+    });
+  }, { threshold: 0.2 });
+
+  const elements = document.querySelectorAll('.animate') as NodeListOf<Element>;
+
+  elements.forEach((el: Element) => {
+    observer.observe(el);
+  });
+
+}
 
 }
--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -10,4 +10,60 @@ import { RouterModule } from '@angular/router';
 })
 export class Home {
 
+ngAfterViewInit(): void {
+
+
+  const statsSection = document.querySelector('.stats-section');
+  const counters = document.querySelectorAll('.counter');
+
+  const counterObserver = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+
+        counters.forEach(counter => {
+          const target = +counter.getAttribute('data-target')!;
+          let count = 0;
+          const increment = target / 100;
+
+          const updateCount = () => {
+            if (count < target) {
+              count += increment;
+              counter.textContent = Math.ceil(count) + '+';
+              requestAnimationFrame(updateCount);
+            } else {
+              counter.textContent = target + '+';
+            }
+          };
+
+          updateCount();
+        });
+
+        // Stop observing after animation runs once
+        counterObserver.unobserve(entry.target);
+      }
+    });
+  }, { threshold: 0.4 });
+
+  if (statsSection) {
+    counterObserver.observe(statsSection);
+  }
+
+
+  const inspirationSection = document.querySelector('.inspiration-section');
+
+  const inspirationObserver = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        inspirationSection?.classList.add('show');
+      }
+    });
+  }, { threshold: 0.3 });
+
+  if (inspirationSection) {
+    inspirationObserver.observe(inspirationSection);
+  }
+
+}
+
+
 }
